5b89b654b873929e053b79dc4fdda5d738c28b47
Jocelyne Jones <gothling@jocely.net>
2024-09-25 11:13:53 +0100

    added form page

---
+++ my-app/src/app/form/page.js
@@ -0,0 +1,12 @@
+"use client";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { supabase } from '../../lib/supabaseClient';
+
+export default function FormPage() {
+  return(
+    <>
+      Hello world
+    </>
+  )
+}

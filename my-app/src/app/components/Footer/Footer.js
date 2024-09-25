4b378e9eb96deb4d30f8e713dab1e23761507a0d
Âdil <therealadil@icloud.com>
2024-09-25 21:27:23 +0100

    style footer

--- my-app/src/app/components/Footer/Footer.js
+++ my-app/src/app/components/Footer/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'; // Import the GitHub icon
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>© 2024 The Mad Hackers. All rights reserved.</p>
+            <p className="footer-text">Made by The Mad Hackers. © 2030</p>
+            <a 
+                href="https://github.com/SchoolOfCode/final-project-the-mad-hackers/" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="github-link"
+            >
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
         </footer>
     );
 };
